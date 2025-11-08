f566cb977f69d291dc8a2a3679562326342b004e
Jovansa Putra Laksana <ahmfarisi@gmail.com>
2025-11-08 10:27:16 +0700

    Update Project Files

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_puntikayu/detail_screen.dart';
+import 'package:flutter_puntikayu/screens/detail_screen.dart';
 
 void main() {
   runApp(const MainApp());
